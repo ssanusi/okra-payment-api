fcf0ceec71f382ebdd4dffb49e13fabaea998539
Sulaiman Sanusi <sulaimansanusi@icloud.com>
2022-07-31 00:33:15 +0100

    setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb://root:pass12345@mongodb:27017/payment?serverSelectionTimeoutMS=2000&authSource=admin`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe, BadRequestException } from '@nestjs/common';
+
+const logger: Logger = new Logger('Main');
+const PORT = process.env.SERVER_PORT || 8800;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (): BadRequestException =>
+        new BadRequestException('Validation error'),
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
 }
 bootstrap();
